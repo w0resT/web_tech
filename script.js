7c7d067a5b49d810a9488b8d5fd5c2becf2af9fa
w0resT <71945861+w0resT@users.noreply.github.com>
2021-09-22 18:26:36 +0300

    Added files

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+    //always start with console output to see if it's working
+    //unless you've done it 100 times and 100% sure it is.
+    console.log('hello mfker');
+	bind_ui_actions();
+}
+
+function bind_ui_actions() {
+	// Adding events for focus & unfocus
+	document.getElementById("calc").addEventListener("focus", calc_of_wages);
+}
+
+function calc_of_wages() {
+	// Getting money per hour and how much this guy is worked today
+	var money_input = document.getElementById("money_ph").value;
+	var hours_input = document.getElementById("hours").value;
+	
+	// Calculating his salary
+	var salary = Math.abs(money_input * hours_input);
+	
+	// Print his 'big' salary
+	var span = document.getElementById("wages");
+	span.innerHTML = salary;
+}
